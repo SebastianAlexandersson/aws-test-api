70c0f8a7fe06d27edc86d3a24d31d269a0b8986a
Sebastian Alexandersson <fortheloveofgood@gmail.com>
2021-02-27 23:36:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 
 app.route('/')
   .all((req, res) => {
-    res.send(`secrets: ${process.env.USERNAME}`)
+    res.status(200).send();
   });
+
+app.route('/node-test')
+  .all((req, res) => {
+    res.send(`secrets: ${process.env.USERNAME}`);
+  })
+
 
 app.listen(7244, () => console.log('server up on port 7244'));
